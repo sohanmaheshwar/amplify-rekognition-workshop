3ba47d4c4c708b8c0a503aa62880c04dad570359
Sohan <1119120+sohanmaheshwar@users.noreply.github.com>
2020-09-04 16:42:31 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+const AWS = require('aws-sdk');
+
+const region = 'ADD-REGION-HERE';
+const poolID = 'ADD-COGNITO-IDENTITY-POOL-ID-HERE';
+
+Amplify.configure(awsconfig);
+
+AWS.config.update({region: region});
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: poolID});
+
+const rek = new AWS.Rekognition();
+
+//const MutationButton = document.getElementById("MutationEventButton");
+const bucket = awsconfig.aws_user_files_s3_bucket;
+
+
+var fileTag = document.getElementById("myFile");
+fileTag.addEventListener("change", function() {
+    Storage.put(this.files[0].name, this.files[0])
+    .then (result => {
+        console.log(result);
+        identify (this.files[0].name);
+    }) 
+    .catch(err => console.log(err));
+  });
+  
+  
+function identify (filename) {
+    const params = {
+      Image: {
+        S3Object: {
+          Bucket: bucket,
+          Name: `public/${filename}`,
+        },
+      },
+      MaxLabels: 10,
+      MinConfidence: 75,
+    };
+
+    Storage.list('/')
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+
+    console.log(`Analyzing file: https://s3.amazonaws.com/${bucket}/${filename}`);
+
+    return new Promise((resolve, reject) => {
+      rek.detectLabels(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          return reject(new Error(err));
+        }
+
+        var labelArray = [];
+        data.Labels.forEach(function(item) {
+          labelArray.push(item.Name + "<br>")
+        })
+
+        console.log(labelArray);
+        //ReactDOM.render(labelArray, document.getElementById('root'));
+        var myDiv = document.getElementById("Labels");
+        myDiv.innerHTML = labelArray;
+        return resolve(data.Labels);
+      });
+    });
+}
